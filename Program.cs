9d3901ce4ad8e115532bd1a752b955e38029f1ff
AenuHub <jesuskane92@gmail.com>
2024-09-30 10:53:02 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace W1_CodingGoal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mehmet Özsığırtmaç");
+            Console.WriteLine("Patika+ ile birlikte yazılım sektörüne geçiş yapmak istiyorum.");
+            Console.WriteLine("Bu nedenle önümüzdeki haftalarda verilen eğitimleri sıkı sıkıya takip edip, bolca pratik yaparak kendimi geliştirmeyi hedefliyorum.");
+
+            Console.Read();
+        }
+    }
+}
